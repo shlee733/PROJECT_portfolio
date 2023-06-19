bfac7c955b9ebf6592177e7f35e7a836bffc57f4
lsh <pttc733@gmail.com>
2023-06-19 14:02:59 +0900

    ver 1.2_페이지 이동효과

--- common.js
+++ common.js
@@ -1,32 +1,54 @@
 const entire = document.querySelector(".entire");
-const sections = document.querySelectorAll(".sec");
+const sections = entire.querySelectorAll(".sec");
 
-let currentSection = 0;
+// let currentSection = 0;
+let btnRight = document.querySelectorAll(".btn_right");
+btnRight.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    //클릭한 요소의 data-num을 가져온다.
+    let currentSection = e.target.dataset.num;
+    // console.log(currentSection);
+    moveNext(currentSection);
+  });
+});
 
-function moveNext() {
-  if (currentSection < sections.length - 1) {
-    currentSection++;
-    const moveX = currentSection * -100;
-    const moveY = currentSection * 20;
-    // entire.style.transform = `translate(${moveX}vw, ${moveY}vh)`;
-    entire.style.transform = `translateY(${moveY}vh)`;
+let btnLeft = document.querySelectorAll(".btn_left");
+btnLeft.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    let currentSection = e.target.dataset.num;
+    movePrev(currentSection);
+  });
+});
 
-    setTimeout(() => {
-      entire.style.transform = `translate(${moveX}vw, ${moveY}vh)`;
-    }, 500);
-  }
+function moveNext(currentSection) {
+  //section위 위치 및 넓이 정보를 가져온다.
+  let section = entire.querySelector(".home");
+  let left = section.getBoundingClientRect().left;
+  let top = section.getBoundingClientRect().top;
+  let width = section.getBoundingClientRect().width;
+  let height = section.getBoundingClientRect().height / 5;
+  console.log(currentSection, width);
+  console.log(left, top);
+
+  let moveY = currentSection * height;
+  let moveX = currentSection * -width;
+  entire.style.transform = `translate(${left}px, ${moveY}px)`;
+  setTimeout(() => {
+    entire.style.transform = `translate(${moveX}px, ${moveY}px)`;
+  }, 500);
 }
 
-function movePrev() {
-  if (currentSection > 0) {
-    currentSection--;
-    const moveX = currentSection * 100; // 섹션 너비만큼 이동
-    const moveY = currentSection * -20; // 섹션 높이만큼 이동
-    // entire.style.transform = `translate(${moveX}vw, ${moveY}vh)`;
-    entire.style.transform = `translateY(${moveY}vh)`;
+function movePrev(currentSection) {
+  let section = entire.querySelector(".home");
+  let left = section.getBoundingClientRect().left;
+  let top = section.getBoundingClientRect().top;
+  let width = section.getBoundingClientRect().width;
+  let height = section.getBoundingClientRect().height / 5;
 
-    setTimeout(() => {
-      entire.style.transform = `translate(${moveX}vw, ${moveY}vh)`;
-    }, 500);
-  }
+  let moveY = currentSection * height;
+  let moveX = currentSection * -width;
+  entire.style.transform = `translate(${moveX}px, ${top}px)`;
+  setTimeout(() => {
+    entire.style.transform = `translate(${moveX}px, ${moveY}px)`;
+  }, 500);
 }
